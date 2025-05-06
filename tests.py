bad587dd58e6547ef07b3ab1b9011645cce54ff3
DHarrison <dharrison5@ewu.edu>
2025-05-06 16:40:11 -0700

    CI integration 1st commit

--- tests.py
+++ tests.py
@@ -1,17 +1,18 @@
 import functions
-import unittest
-class TestFunctions(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(functions.add(2, 3), 5)
-        self.assertEqual(functions.add(-1, 1), 0)
-        self.assertEqual(functions.add(0, 0), 0)
-        self.assertEqual(functions.add(-5, -3), -8)
 
-    def test_subtract(self):
-        self.assertEqual(functions.subtract(5, 3), 2)
-        self.assertEqual(functions.subtract(0, 1), -1)
-        self.assertEqual(functions.subtract(-1, -1), 0)
-        self.assertEqual(functions.subtract(10, 20), -10)
+def test_add():
+    assert functions.add(2, 3) == 5
+    assert functions.add(-1, 1) == 0
+    assert functions.add(0, 0) == 0
+    assert functions.add(-5, -3) == -8
+
+def test_subtract():
+    assert functions.subtract(5, 3) == 2
+    assert functions.subtract(0, 1) == -1
+    assert functions.subtract(-1, -1) == 0
+    assert functions.subtract(10, 20) == -10
+
 
 if __name__ == "__main__":
-    unittest.main()
+    test_add()
+    test_subtract()
